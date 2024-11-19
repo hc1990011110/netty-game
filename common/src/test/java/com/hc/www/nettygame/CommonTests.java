0d8c68db55746f6750445904a260137f4c383e70
huangchao <332330633@qq.com>
2024-11-19 23:12:35 +0800

    拆分出common

---
+++ common/src/test/java/com/hc/www/nettygame/CommonTests.java
@@ -0,0 +1,13 @@
+package com.hc.www.nettygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
